cc7cc8cd6a16347eedb1b8ccc85a77c272196761
Jonathan TISSEAU <jonathan.tisseau@86dev.fr>
2018-05-10 16:22:19 +0200

    documentation

--- src/Enums/ForeignKeyAction.php
+++ src/Enums/ForeignKeyAction.php
@@ -20,21 +20,25 @@ abstract class ForeignKeyAction extends \PHPTools\Enum
 	/**
 	 * The MySQL Server rejects the delete or update operation for the parent table if there is a related foreign key value in the referenced table.
 	 * A keyword from standard SQL. In MySQL, equivalent to RESTRICT.
-	 * Some database systems have deferred checks, and NO ACTION is a deferred check. In MySQL, foreign key constraints are checked immediately, so NO ACTION is the same as RESTRICT. 
+	 * Some database systems have deferred checks, and NO ACTION is a deferred check. In MySQL, foreign key constraints are checked immediately, so NO ACTION is the same as RE
+	 * Note: Cascaded foreign key actions do not activate triggers.STRICT. 
 	 */
 	const NOACTION = 'NO ACTION';
 	/**
 	 * Rejects the delete or update operation for the parent table. Specifying RESTRICT (or NO ACTION) is the same as omitting the ON DELETE or ON UPDATE clause.
+	 * Note: Cascaded foreign key actions do not activate triggers.
 	 */
 	const RESTRICT = 'RESTRICT';
 	/**
 	 * This action is recognized by the MySQL parser, but both InnoDB and NDB reject table definitions containing ON DELETE SET DEFAULT or ON UPDATE SET DEFAULT clauses.
+	 * Note: Cascaded foreign key actions do not activate triggers.
 	 */
 	const SETDEFAULT = 'SET DEFAULT';
 	/**
 	 * Delete or update the row from the parent table, and set the foreign key column or columns in the child table to NULL.
 	 * Both ON DELETE SET NULL and ON UPDATE SET NULL clauses are supported.
-	 * Note: If you specify a SET NULL action, make sure that you have not declared the columns in the child table as NOT NULL. 
+	 * Note: If you specify a SET NULL action, make sure that you have not declared the columns in the child table as NOT NULL.
+	 * Note: Cascaded foreign key actions do not activate triggers.
 	 */
 	const SETNULL = 'SET NULL';
 }
